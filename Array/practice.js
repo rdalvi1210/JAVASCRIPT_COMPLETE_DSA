12e4f3700eb8f22d169bd2584b292125baca6154
Ramchandra Dalvi <rdalvi1210@gmail.com>
2025-08-24 22:04:16 +0530

    revised questions

--- Array/practice.js
+++ Array/practice.js
@@ -1,14 +1,27 @@
-const arr = [1, 2, 3, 4, 5];
-const k = 2;
+// const arr = [1, 2, 3, 4, 5];
+// const k = 2;
 
-function rotation(k, arr) {
-  for (let j = 1; j <= k; j++) {
-    let lastElement = arr[arr.length - 1];
-    for (i = arr.length - 1; i > 0; i--) {
-      arr[i] = arr[i - 1];
-    }
-    arr[0] = lastElement;
+// function rotation(k, arr) {
+//   for (let j = 1; j <= k; j++) {
+//     let lastElement = arr[arr.length - 1];
+//     for (i = arr.length - 1; i > 0; i--) {
+//       arr[i] = arr[i - 1];
+//     }
+//     arr[0] = lastElement;
+//   }
+//   return arr;
+// }
+// console.log(rotation(k, arr));
+
+
+let str = "Hello, World!";
+
+function reverseString(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
   }
-  return arr;
+  return reversed;
 }
-console.log(rotation(k, arr));
+
+console.log(reverseString(str)); // Output: !dlroW ,olleH
